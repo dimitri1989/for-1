b909586116b8165b4879933dba94114b814c5c17
dimitri1989 <dimitri.kvarelashvili@gmail.com>
2023-06-19 13:12:20 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let x = 0; x<array.length; x++){
+    array.splice(x)
+}
+console.log(array);
+/*for(let x = 0; x<array.length; x++){
+     array.pop()
+ }
+ console.log(array);*/
+
+
